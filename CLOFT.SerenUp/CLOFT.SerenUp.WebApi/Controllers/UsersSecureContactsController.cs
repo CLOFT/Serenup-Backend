a036d5777507c09cec0666a0d3fb891168c7a7a0
Mateo Sheshi <mateo.sheshi@stud.tecnicosuperiorekennedy.it>
2022-06-30 14:40:46 +0200

    feat : added alarm POST api

--- CLOFT.SerenUp/CLOFT.SerenUp.WebApi/Controllers/UsersSecureContactsController.cs
+++ CLOFT.SerenUp/CLOFT.SerenUp.WebApi/Controllers/UsersSecureContactsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CLOFT.SerenUp.WebApi.Controllers;
 
-[Route("[controller]")]
+[Route("api/[controller]")]
 [ApiController]
 public class UsersSecureContactsController : ControllerBase
 {
