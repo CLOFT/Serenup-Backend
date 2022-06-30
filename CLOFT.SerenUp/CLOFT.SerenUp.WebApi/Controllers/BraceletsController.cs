d3208da9c7055213b6c14df162e071d36d81819e
Mateo Sheshi <mateo.sheshi@stud.tecnicosuperiorekennedy.it>
2022-06-30 14:52:37 +0200

    chore : added mock getbyid for bracelets

---
+++ CLOFT.SerenUp/CLOFT.SerenUp.WebApi/Controllers/BraceletsController.cs
@@ -0,0 +1,21 @@
+﻿using CLOFT.SerenUp.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CLOFT.SerenUp.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BraceletsController : ControllerBase
+{
+    // GET /id
+    [HttpGet("{id}")]
+    public Task<Bracelet> GetById(Guid id)
+    {
+        return Task.FromResult(new Bracelet
+        {
+            SerialNumber = id,
+            Color = "red",
+            Username = "test"
+        });
+    }
+}
