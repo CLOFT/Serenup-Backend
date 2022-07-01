a02fc97cef9689716314334d7937ceca8e807a16
Mateo Sheshi <mateo.sheshi@stud.tecnicosuperiorekennedy.it>
2022-07-01 09:49:44 +0200

    chore : mock get bracelets

--- CLOFT.SerenUp/CLOFT.SerenUp.WebApi/Controllers/BraceletsController.cs
+++ CLOFT.SerenUp/CLOFT.SerenUp.WebApi/Controllers/BraceletsController.cs
@@ -7,6 +7,27 @@ namespace CLOFT.SerenUp.WebApi.Controllers;
 [ApiController]
 public class BraceletsController : ControllerBase
 {
+    private readonly string[] _colors = {"red", "green", "black"};
+    public const int BRACELETS_NUMBER = 100;
+
+// GET /
+    [HttpGet]
+    public Task<IEnumerable<Bracelet>> Get()
+    {
+        // MOCK Bracelets
+        var list = new List<Bracelet>();
+        for (int i = 0; i < BRACELETS_NUMBER; i++)
+        {
+            list.Add(new Bracelet
+            {
+                Color = _colors[new Random().Next(3)],
+                SerialNumber = Guid.NewGuid()
+            });
+        }
+
+        return Task.FromResult<IEnumerable<Bracelet>>(list);
+    }
+
     // GET /id
     [HttpGet("{id}")]
     public Task<Bracelet> GetById(Guid id)
