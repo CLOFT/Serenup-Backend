a3e640dfcf482451655cc2038eb3e2202a8035c0
Mateo Sheshi <mateo.sheshi@stud.tecnicosuperiorekennedy.it>
2022-07-06 14:27:28 +0200

    fix(bracelets) : added RepoDB bootstrap and commented userSecrets

--- CLOFT.SerenUp/CLOFT.SerenUp.WebApi/Program.cs
+++ CLOFT.SerenUp/CLOFT.SerenUp.WebApi/Program.cs
@@ -1,8 +1,14 @@
 using System.Reflection;
+using CLOFT.SerenUp.AppCore.Interfaces.Repositories;
+using CLOFT.SerenUp.AppCore.Interfaces.Services;
+using CLOFT.SerenUp.AppCore.Services;
+using CLOFT.SerenUp.Infrastructure.Repositories;
+
+RepoDb.PostgreSqlBootstrap.Initialize();
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+//builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
 
 // Add services to the container.
 
@@ -10,6 +16,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Config singleton
+builder.Services.AddSingleton<IBraceletsRepository, BraceletsRepository>();
+builder.Services.AddSingleton<IBraceletsService, BraceletsService>();
+
 
 // Add AWS Lambda Hosting
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
