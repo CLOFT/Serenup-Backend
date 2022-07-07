2ca9ee1ac466f9319e5ae2bf44523626457314f7
Mateo Sheshi <mateo.sheshi@stud.tecnicosuperiorekennedy.it>
2022-07-07 15:51:16 +0200

    chore(bracelet) : changed username field to nullable

--- CLOFT.SerenUp/CLOFT.SerenUp.Domain/Models/Bracelet.cs
+++ CLOFT.SerenUp/CLOFT.SerenUp.Domain/Models/Bracelet.cs
@@ -6,7 +6,7 @@ namespace CLOFT.SerenUp.Domain.Models;
 public class Bracelet
 {
     public Guid SerialNumber { get; set; }
-    public string Username { get; set; }
+    public string? Username { get; set; }
     public string Color { get; set; }
 
     public Bracelet()
